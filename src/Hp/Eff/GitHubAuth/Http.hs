b77f5d16a995439a6ef4ec178a739771fdba7475
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-04-06 22:01:35 -0400

    fix partial pattern match

--- src/Hp/Eff/GitHubAuth/Http.hs
+++ src/Hp/Eff/GitHubAuth/Http.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes, UndecidableInstances #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -12,8 +12,10 @@ import Hp.Eff.HttpClient                           (HttpClient)
 import Hp.GitHub                                   (gitHubGetUser, gitHubPostLoginOauthAccessToken)
 import Hp.GitHub.ClientId                          (GitHubClientId)
 import Hp.GitHub.ClientSecret                      (GitHubClientSecret)
+import Hp.GitHub.Code                              (GitHubCode)
 import Hp.GitHub.PostLoginOauthAccessTokenResponse (GitHubPostLoginOauthAccessTokenResponse)
 import Hp.GitHub.Response                          (GitHubResponse(..))
+import Hp.GitHub.User                              (GitHubUser)
 
 import Control.Effect
 import Control.Effect.Carrier
@@ -34,40 +36,55 @@ instance
     -> GitHubAuthCarrier "http" env m a
   eff = \case
     L (GitHubAuth code next) ->
-      GitHubAuthCarrier $ do
-        clientId :: GitHubClientId <-
-          asks @env (^. typed)
+      GitHubAuthCarrier (doGitHubAuth @env code) >>= next
 
-        clientSecret :: GitHubClientSecret <-
-          asks @env (^. typed)
+    R other ->
+      GitHubAuthCarrier (eff (handleCoercible other))
 
-        let
-          doPost ::
-               m (Either SomeException (GitHubResponse GitHubPostLoginOauthAccessTokenResponse))
-          doPost =
-            gitHubPostLoginOauthAccessToken
-              clientId
-              clientSecret
-              code
-              -- TODO type safe link, and get this from the environment
-              (Just "http://localhost:8000/login/github")
-              -- TODO send random state
-              Nothing
+doGitHubAuth ::
+     ∀ env sig m.
+     ( Carrier sig m
+     , HasType GitHubClientId env
+     , HasType GitHubClientSecret env
+     , Member HttpClient sig
+     , Member (Reader env) sig
+     )
+  => GitHubCode
+  -> m (Maybe GitHubUser)
+doGitHubAuth code = do
+  clientId :: GitHubClientId <-
+    asks @env (^. typed)
 
-        doPost >>= \case
-          Left _ ->
-            runGitHubAuthHttp (next Nothing)
+  clientSecret :: GitHubClientSecret <-
+    asks @env (^. typed)
 
-          Right (GitHubResponseError _) ->
-            runGitHubAuthHttp (next Nothing)
+  let
+    doPost ::
+          m (Either SomeException (GitHubResponse GitHubPostLoginOauthAccessTokenResponse))
+    doPost =
+      gitHubPostLoginOauthAccessToken
+        clientId
+        clientSecret
+        code
+        -- TODO type safe link, and get this from the environment
+        (Just "http://localhost:8000/login/github")
+        -- TODO send random state
+        Nothing
 
-          Right (GitHubResponseSuccess response) ->
-            gitHubGetUser (response ^. field @"access_token") >>= \case
-              Left _ ->
-                runGitHubAuthHttp (next Nothing)
+  doPost >>= \case
+    Left _ ->
+      pure Nothing
 
-              Right user ->
-                runGitHubAuthHttp (next (Just user))
+    Right (GitHubResponseError _) ->
+      pure Nothing
+
+    Right (GitHubResponseSuccess response) ->
+      gitHubGetUser (response ^. field @"access_token") >>= \case
+        Left _ ->
+          pure Nothing
+
+        Right user ->
+          pure (Just user)
 
 runGitHubAuthHttp ::
      ∀ env m a.
