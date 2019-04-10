dad5511cc519bb62dc67e7734ffc0c3646279b8c
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-04-09 20:26:09 -0400

    move ManagePoll db carrier into its own module

--- src/Hp/Eff/ManagePoll.hs
+++ src/Hp/Eff/ManagePoll.hs
@@ -1,31 +1,17 @@
 -- TODO rename to PersistPoll
 
-{-# LANGUAGE UndecidableInstances #-}
-
 module Hp.Eff.ManagePoll
-  ( ManagePoll
+  ( ManagePoll(..)
   , getPoll
   , savePoll
-  , ManagePollDBC(..)
   ) where
 
-import Data.Aeson (toJSON)
 import Control.Effect
 import Control.Effect.Carrier
-import Control.Effect.Sum
-
-import qualified Data.Aeson as A
-import qualified Data.ByteString.Lazy as BL
-import qualified Hasql.Session as H
-import qualified Hasql.Statement as H
-import qualified Hasql.Encoders as E
-import qualified Hasql.Decoders as D
 
 import Hp.Eff.FirstOrder (FirstOrderEffect(..))
-import Hp.Eff.DB
 import Hp.Poll
 import Hp.PollId (PollId(..))
-import Hp.PollFormElement (PollFormElement)
 
 data ManagePoll (m :: Type -> Type) (k :: Type) where
   GetPoll :: PollId -> (Maybe (Poll PollId) -> k) -> ManagePoll m k
@@ -40,50 +26,3 @@ savePoll poll =
 
 getPoll :: (Carrier sig m, Member ManagePoll sig) => PollId -> m (Maybe (Poll PollId))
 getPoll pollId = send $ GetPoll pollId pure
-
-newtype ManagePollDBC m a
-  = ManagePollDBC
-  { unManagePollDBC :: m a
-  } deriving newtype (Functor, Applicative, Monad, MonadIO)
-
-instance ( Carrier sig m
-         , Member DB sig
-         ) => Carrier (ManagePoll :+: sig) (ManagePollDBC m) where
-  eff = ManagePollDBC . \case
-    L (SavePoll p k) -> do
-      let sql = "INSERT INTO polls (form, end_time) VALUES ($1, $2) RETURNING id"
-      runDB (H.statement p (H.Statement sql encodePoll (D.singleRow decodePollId) True)) >>= \case
-        Left err -> error (show err)
-        -- TODO handle error
-        Right pollId -> unManagePollDBC $ k pollId
-    L (GetPoll pollId k) -> do
-      let sql = "SELECT form, end_time FROM polls WHERE id = $1"
-      runDB (H.statement pollId (H.Statement sql encodePollId (D.rowMaybe (decodePoll pollId)) True)) >>= \case
-        Left err -> error (show err)
-        -- TODO handle error
-        Right x -> unManagePollDBC $ k x
-    R other -> eff (handleCoercible other)
-    where
-
-encodePoll :: E.Params (Poll ())
-encodePoll = mconcat
-  [ toJSON . elements >$< E.param E.jsonb
-  , endTime >$< E.param E.timestamptz
-  ]
-
-decodePoll :: PollId -> D.Row (Poll PollId)
-decodePoll pollId = Poll
-  <$> pure pollId
-  <*> parseForm
-  <*> D.column D.timestamptz
-  where
-    parseForm :: D.Row (Seq PollFormElement)
-    parseForm = D.column
-      . D.jsonbBytes
-      $ over _Left (view packed) . A.eitherDecode . BL.fromStrict
-
-encodePollId :: E.Params PollId
-encodePollId = coerce (E.param E.uuid)
-
-decodePollId :: D.Row PollId
-decodePollId = D.column (PollId <$> D.uuid)
---
+++ src/Hp/Eff/ManagePoll/DB.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Hp.Eff.ManagePoll.DB
+  ( ManagePollDBC(..)
+  ) where
+
+import Hp.Eff.DB
+import Hp.Eff.ManagePoll  (ManagePoll(..))
+import Hp.Poll
+import Hp.PollFormElement (PollFormElement)
+import Hp.PollId          (PollId(..))
+
+import Control.Effect
+import Control.Effect.Carrier
+import Control.Effect.Sum
+import Data.Aeson             (toJSON)
+
+import qualified Data.Aeson           as A
+import qualified Data.ByteString.Lazy as BL
+import qualified Hasql.Decoders       as D
+import qualified Hasql.Encoders       as E
+import qualified Hasql.Session        as H
+import qualified Hasql.Statement      as H
+
+
+newtype ManagePollDBC m a
+  = ManagePollDBC
+  { unManagePollDBC :: m a
+  } deriving newtype (Functor, Applicative, Monad, MonadIO)
+
+instance ( Carrier sig m
+         , Member DB sig
+         ) => Carrier (ManagePoll :+: sig) (ManagePollDBC m) where
+  eff = ManagePollDBC . \case
+    L (SavePoll p k) -> do
+      let sql = "INSERT INTO polls (form, end_time) VALUES ($1, $2) RETURNING id"
+      runDB (H.statement p (H.Statement sql encodePoll (D.singleRow decodePollId) True)) >>= \case
+        Left err -> error (show err)
+        -- TODO handle error
+        Right pollId -> unManagePollDBC $ k pollId
+    L (GetPoll pollId k) -> do
+      let sql = "SELECT form, end_time FROM polls WHERE id = $1"
+      runDB (H.statement pollId (H.Statement sql encodePollId (D.rowMaybe (decodePoll pollId)) True)) >>= \case
+        Left err -> error (show err)
+        -- TODO handle error
+        Right x -> unManagePollDBC $ k x
+    R other -> eff (handleCoercible other)
+    where
+
+encodePoll :: E.Params (Poll ())
+encodePoll = mconcat
+  [ toJSON . elements >$< E.param E.jsonb
+  , endTime >$< E.param E.timestamptz
+  ]
+
+decodePoll :: PollId -> D.Row (Poll PollId)
+decodePoll pollId = Poll
+  <$> pure pollId
+  <*> parseForm
+  <*> D.column D.timestamptz
+  where
+    parseForm :: D.Row (Seq PollFormElement)
+    parseForm = D.column
+      . D.jsonbBytes
+      $ over _Left (view packed) . A.eitherDecode . BL.fromStrict
+
+encodePollId :: E.Params PollId
+encodePollId = coerce (E.param E.uuid)
+
+decodePollId :: D.Row PollId
+decodePollId = D.column (PollId <$> D.uuid)
